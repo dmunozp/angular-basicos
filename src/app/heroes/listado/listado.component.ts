ea500e9103b58d65f7d16993a024f27eff2226df
David Munoz <dmp.david@gmail.com>
2021-11-15 12:28:37 +0100

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  heroeBorrado: string = '';
+
+  constructor() { 
+    console.log("constructor");
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
